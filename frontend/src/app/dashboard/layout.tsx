a5168d8763eb191c3b6858a5e773cddc422cebac
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-02-20 23:25:28 +0200

    Fix: fix side nav overflow problem

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-2/12 md:w-3/12 flex flex-col items-center md:items-start md:pl-4 pt-4">
+      <div className="w-2/12 md:w-3/12 flex flex-col items-center md:items-start md:pl-4 pt-4 overflow-scroll">
         <Link href="/" className="mb-2 flex gap-2 items-center  text-gray-800">
           <IoSchoolSharp className="size-6" />
           <span className="hidden md:block font-bold text-sm lg:text-base">
